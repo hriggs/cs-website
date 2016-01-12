2d4534432f71187a11168fa57912b58b9078f9d8
Hannah Riggs <hkriggs128@gmail.com>
2016-01-12 15:55:29 -0500

    JS NavBar highlighting started.

--- templates/massbaycsdepartment/js/scripts.js
+++ templates/massbaycsdepartment/js/scripts.js
@@ -10,12 +10,12 @@ jQuery(document).ready(function($){
     	//jQuery('.navbar-inner').width(jQuery(window).width());
         //jQuery('#nav-wrapper').width(jQuery(window).width());
         
-        console.log("nav inner width: " + jQuery('.navbar-inner').width());
+        /*console.log("nav inner width: " + jQuery('.navbar-inner').width());
         console.log("nav: " + jQuery('#nav').width());
         console.log("nav wrapper width: " + jQuery('#nav-wrapper').width());
         console.log("body width: " + jQuery('body').width());
         console.log("window width: " + jQuery(window).width());
-        console.log("document width: " + jQuery(document).width());
+        console.log("document width: " + jQuery(document).width());*/
 	});
     
 
@@ -33,12 +33,48 @@ jQuery(document).ready(function($){
         
 		//jQuery('.navbar-inner').width(jQuery(window).width());
         //jQuery('#nav-wrapper').width(jQuery(window).width());
-        console.log("nav inner width: " + jQuery('.navbar-inner').width());
+        /*console.log("nav inner width: " + jQuery('.navbar-inner').width());
         console.log("nav: " + jQuery('#nav').width());
         console.log("nav wrapper width: " + jQuery('#nav-wrapper').width());
         console.log("body width: " + jQuery('body').width());
         console.log("window width: " + jQuery(window).width());
-        console.log("document width: " + jQuery(document).width());
+        console.log("document width: " + jQuery(document).width());*/
 	});
+    
+    // highlight navbar
+    highlightNav();
 
 });
+
+// list of child URLs
+var degrees = ["/computer-information-systems", "/computer-science", "/information-systems-technology-management-management", "/information-systems-technology-management-technology", "/cyber-security", "/advanced-cyber-security", "/information-technology", "/technology-support", "/web-designer", "/web-developer", "/web-master"];
+
+var courses = ["/shopping-cart-experience", "/cs-101", "/icreate"];
+
+var inits = ["/cs-engineering-club", "/technology-internship-program", "/stem-mentor-program"];
+
+/**
+ * Highlight main navbar at top when on child pages. 
+ */
+function highlightNav() {
+
+	// check which child page on 
+	if (degrees.indexOf(window.location.pathname) > -1) {
+
+        // get parent item in main navbar
+        var parentList = document.getElementsByClassName("item-105");
+        var parent = parentList[0];
+        
+        // add current and active classes to parent
+        parent.className += " current active";
+    } else if (courses.indexOf(window.location.pathname) > -1) {
+    
+    	// get parent item in main navbar
+        var parentList = document.getElementsByClassName("item-106");
+        var parent = parentList[0];
+        
+        // add current and active classes to parent
+        parent.className += " current active";
+    }
+
+}
